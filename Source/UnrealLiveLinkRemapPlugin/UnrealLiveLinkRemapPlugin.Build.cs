18d45ca6687512a4297129e789b5facedb99bd5d
Andrea Ruzzenenti <ruzzenenti.a@gmail.com>
2023-03-09 00:08:22 +0100

    updates dependency

--- Source/UnrealLiveLinkRemapPlugin/UnrealLiveLinkRemapPlugin.Build.cs
+++ Source/UnrealLiveLinkRemapPlugin/UnrealLiveLinkRemapPlugin.Build.cs
@@ -38,6 +38,14 @@ public class UnrealLiveLinkRemapPlugin : ModuleRules
 				"RadicalSocketIOLib", "Json",
 				"LevelEditor",
 				"EditorStyle",
+				"EditorSubsystem",
+				"Slate",
+				"SlateCore",
+				"UnrealEd",
+				"Blutility",
+				"UMG",
+				"UMGEditor",
+				"Projects"
 			}
 		);
 
